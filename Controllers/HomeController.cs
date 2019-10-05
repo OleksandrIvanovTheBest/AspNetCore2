5f35ccfeab0791bab4f2275d594a04af499751c1
Ivanov Oleksandr <sanyaivanov2007@gmail.com>
2019-10-05 16:22:12 +0300

    Initial configuration

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Settings.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action"
+            });
+        }
+    }
+}
